0c5f5f87969b74e6003e568a91e1ead79b7c9386
Stuart John Atkinson <stuart.john.atkinson@hotmail.com>
2024-08-14 20:01:41 +0100

    Revert "autopilot proposal (#2)" (#3)
    This reverts commit f33ead419fd8c91ed17b4ca16b04947c81491243.

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.windows.create({
-        url: 'popup.html',
-        type: 'popup',
-        width: 800,
-        height: 600
-    });
-});
